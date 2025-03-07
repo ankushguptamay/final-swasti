ee507c046b4108f923c43264be4bcdc2f9753575
Ankush Gupta <ankushgupta9675@gmail.com>
2025-03-07 14:25:28 +0530

    wallet created for existing user

--- Util/mongoConnection.js
+++ Util/mongoConnection.js
@@ -20,24 +20,24 @@ const connectDB = (uri) => {
 // await mongoose.connection.db.dropDatabase();
 // console.log("Database dropped");
 
-async function createWallet() {
-  try {
-    const result = await User.find();
-    let num = 0;
-    for (let i = 0; i < result.length; i++) {
-      await Wallet.findOneAndUpdate(
-        { userId: result[i]._id },
-        { updatedAt: new Date() },
-        { upsert: true, new: true, setDefaultsOnInsert: true }
-      );
-      num = i;
-    }
-    console.log(`${num} done.`);
-  } catch (error) {
-    console.error("Error updating documents:", error);
-  }
-}
+// async function createWallet() {
+//   try {
+//     const result = await User.find();
+//     let num = 0;
+//     for (let i = 0; i < result.length; i++) {
+//       await Wallet.findOneAndUpdate(
+//         { userId: result[i]._id },
+//         { updatedAt: new Date() },
+//         { upsert: true, new: true, setDefaultsOnInsert: true }
+//       );
+//       num = i;
+//     }
+//     console.log(`${num} done.`);
+//   } catch (error) {
+//     console.error("Error updating documents:", error);
+//   }
+// }
 
-createWallet();
+// createWallet();
 
 export { connectDB };
