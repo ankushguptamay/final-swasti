01ba7cc60ee55b72e4fd145dac41c876f8d78986
Ankush Gupta <ankushgupta9675@gmail.com>
2025-04-28 22:10:48 +0530

    port change

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use((err, req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
