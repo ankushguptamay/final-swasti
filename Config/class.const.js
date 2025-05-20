f3148cec465ff1f770cfca2c87c9185d688768e4
Ankush Gupta <ankushgupta9675@gmail.com>
2025-05-20 13:50:55 +0530

    5 min validation added

--- Config/class.const.js
+++ Config/class.const.js
@@ -1,6 +1,6 @@
 export const SERVICE_OFFER = 0; // Default discount (%) applied on service listings
 export const PER_CLASS_PRICE_LIMIT = 500; // Minimum allowed price per prson per day class
-export const MEET_CAN_JOIN_BEFORE = 1380; // 1380 min before class start time
+export const MEET_CAN_JOIN_BEFORE = 5; // 5 min before class start time
 export const FUTURE_CLASS_CREATION = 24; // class should be 24 hours in future
 export const CLASS_CANCELATION_TIME = 24; // class should be cancel before 24 hours unless charge apply
 export const CLASS_BOOKING_TIME = 24; // class should be Booked before 24 hours
